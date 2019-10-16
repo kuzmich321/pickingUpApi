75fea7a596287b9054c2bd9dec5dbd041feb7ea9
kuzmich321 <dota2drot@gmail.com>
2019-10-16 16:27:08 +0300

    this is a first commit for now

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    appHeader: Header
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Open+Sans");
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Open Sans", sans-serif;
+}
+
+#app {
+  height: 100vh;
+  background-color: #141b20;
+}
+
+.container {
+  width: 80%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,97 @@
+<template>
+  <header>
+    <div class="header-wrapper">
+      <div class="search">
+        <input type="text" class="search-form" :placeholder="defaultSearchMsg" />
+        <button type="submit" class="search-btn btn">
+          <i class="fa fa-search"></i>
+        </button>
+      </div>
+      <div class="user-section">
+        <button class="user-btn btn">
+          <i class="fa fa-user-circle" aria-hidden="true"></i>
+        </button>
+        <button class="user-btn btn">
+          <i class="fa fa-comments" aria-hidden="true"></i>
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      defaultSearchMsg: "What are you looking for?"
+    };
+  }
+};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 60px;
+  border-bottom: 1px solid #ef1746;
+  margin: auto;
+  margin-bottom: 30px;
+}
+
+.header-wrapper {
+  width: 80%;
+  height: 100%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.search {
+  width: 50%;
+  height: auto;
+  position: relative;
+  display: flex;
+}
+
+.search-form {
+  width: 100%;
+  border: 3px solid #99cef4;
+  border-right: none;
+  padding: 5px;
+  height: 20px;
+  border-radius: 5px 0 0 5px;
+  outline: none;
+  color: #9dbfaf;
+}
+
+.search-form:focus {
+  color: #142121;
+}
+
+.btn {
+  width: 40px;
+  height: 36px;
+  border: 1px solid #99cef4;
+  background: #99cef4;
+  text-align: center;
+  color: #fff;
+  cursor: pointer;
+  font-size: 20px;
+  text-decoration: none;
+}
+
+.search-btn {
+  border-radius: 0 5px 5px 0;
+}
+
+.user-btn {
+  border-radius: 50%;
+}
+
+.user-btn:hover {
+  background: none;
+  border: none;
+  transition: all 0.3s;
+}
+</style>
